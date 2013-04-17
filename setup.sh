7a7ce64d8476c320469e2eb70f95627ab040f31e
Makoto Sasaki <wanigoya@gmail.com>
2013-04-17 22:01:27 +0900

    symlinkしない対象を追加

--- setup.sh
+++ setup.sh
@@ -2,10 +2,15 @@
 
 cd $(dirname $0)
 
+git submodule init
+git submodule update
+
 for filename in .?*
 do
     [ $filename = ".." ] && continue
     [ $filename = ".git" ] && continue
+    [ $filename = ".gitignore" ] && continue
+    [ $filename = ".gitmodules" ] && continue
 
     ln -is "$PWD/$filename" "$HOME"
 done
